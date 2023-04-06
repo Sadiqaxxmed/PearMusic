b90d70d530953077082a33e447c1f72dd4c7d91f
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-04-06 19:16:22 -0400

    l

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -9,11 +9,11 @@ function NavBar() {
   return (
     <div className="NB-body">
       <div className="music-player-container">
-        <i class="fa-solid fa-shuffle fa-sm" id='symbols'></i>
-        <i class="fa-solid fa-backward fa" id='symbols'></i>
-        <i class="fa-solid fa-play fa-xl" id='symbols-1'></i>
-        <i class="fa-solid fa-forward fa" id='symbols'></i>
-        <i class="fa-solid fa-repeat fa-sm" id='symbols'></i>
+        <i class="fa-solid fa-shuffle fa-sm" id='symbols shuffle'></i>
+        <i class="fa-solid fa-backward fa" id='symbols backward'></i>
+        <i class="fa-solid fa-play fa-xl" id='symbols play'></i>
+        <i class="fa-solid fa-forward fa" id='symbols forward'></i>
+        <i class="fa-solid fa-repeat fa-sm" id='symbols repeat'></i>
       </div>
       <div className='NB-MUSIC-BLOCK'>
         ah
@@ -22,7 +22,6 @@ function NavBar() {
         <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
       </div>
       <div className="NB-Symbol">
-
       </div>
     </div>
   )
