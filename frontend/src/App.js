6538feb552d11b21defd75577e3e917c5e0e6b4d
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-04-08 16:31:38 -0400

    login and signup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import './App.css';
