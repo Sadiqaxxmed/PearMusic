3d1f56d11ee8947bf97fbfb63b357be1c84d90f2
MichaelSal <salariosamichael@gmail.com>
2023-04-08 20:37:31 -0700

    implemented redux for songs/album and added AWS/song post route

--- backend/app/api/song_routes.py
+++ backend/app/api/song_routes.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, jsonify
-from app.models import Song, User
+import mutagen
+from mutagen.wave import WAVE
+from flask import Blueprint, jsonify, redirect
+from flask_login import login_required
+from app.models import Song, User, db
+from app.forms import SongForm
+from .AWS_helpers import get_unique_filename, upload_file_to_AWS
 
 song_routes = Blueprint('song', __name__)
 
@@ -12,3 +17,64 @@ def songs():
     """
     songs = Song.query.all()
     return {'songs': [song.to_dict() for song in songs]}
+
+
+@song_routes.route('/singleSong', methods=['POST'])
+@login_required
+def create_song():
+    form = SongForm()
+
+    def audio_duration(length):
+        length %= 3600
+        mins = length // 60  # calculate in minutes
+        length %= 60
+        seconds = length  # calculate in seconds
+
+        return mins, seconds  # returns the duration
+
+    if form.validate_on_submit():
+
+        song = form.data["mp3file"]
+        coverImage = form.data["coverImage"]
+
+        song.filename = get_unique_filename(song.filename)
+        coverImage.filename = get_unique_filename(coverImage.filename)
+
+        # Upload our song and image to AWS
+        uploadSong = upload_file_to_AWS(song)
+        uploadImage = upload_file_to_AWS(coverImage)
+
+        # If song or upload failed to AWS return error message
+        if "url" not in uploadSong or uploadImage:
+            # if the dictionary doesn't have a url key
+            # it means that there was an error when we tried to upload
+            # so we send back that error message
+            return {"message": "Error uploading file to AWS", "status": 500}
+
+        songURL = uploadSong["url"]
+        imageURL = uploadImage["url"]
+
+        audio = WAVE(songURL)
+
+        audio_info = audio.info
+
+        length = int(audio_info.length)
+        mins, seconds = audio_duration(length)
+        songDuration = f'{mins}.{seconds}'
+
+        print(songDuration)
+
+        new_song = Song(
+            title=form.data['title'],
+            genre=form.data['genre'],
+            coverImage=imageURL,
+            mp3file=songURL,
+            duration=float(songDuration)
+        )
+        db.session.add(new_song)
+        db.session.commit()
+        return redirect("/")
+
+    if form.errors:
+        print(form.errors)
+        return {"message": "Invalid Data", "status": 403}
--- backend/app/forms/song_form.py
+++ backend/app/forms/song_form.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, URL
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from ..api.AWS_helpers import ALLOWED_EXTENSIONS
 
 
 class SongForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     genre = StringField('Genre', validators=[DataRequired()])
-    coverImage = StringField('Cover Image', validators=[DataRequired(), URL()])
-    mp3File = StringField('Mp3 File', validators=[DataRequired(), URL()])
+    coverImage = FileField('Cover Image', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    mp3File = FileField('Mp3 File', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
