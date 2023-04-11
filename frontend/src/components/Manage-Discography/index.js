45d03e47e91269d788b179699fab0755596e1ab0
MichaelSal <salariosamichael@gmail.com>
2023-04-10 23:10:33 -0700

    added dynamic classes for the manage directory containers

--- frontend/src/components/Manage-Discography/index.js
+++ frontend/src/components/Manage-Discography/index.js
@@ -30,26 +30,26 @@ function ManageDiscography() {
   }, [dispatch, userId])
 
 
-  // function MyComponent(numSlides, className) {
-  //   const imageURL = "https://assets.teenvogue.com/photos/615c6f908b261647679498e4/16:9/w_2560%2Cc_limit/GettyImages-1344925419.jpg";
-  //   const imageCount = numSlides;
+  function MyComponent(numSlides, className) {
+    const imageURL = "https://assets.teenvogue.com/photos/615c6f908b261647679498e4/16:9/w_2560%2Cc_limit/GettyImages-1344925419.jpg";
+    const imageCount = numSlides;
 
-  //   const images = [];
-  //   for (let i = 0; i < imageCount; i++) {
-  //     images.push(<SwiperSlide className='MD-songs-carousel-images-div'><img className={className} key={i} src={imageURL} alt={`Image ${i + 1}`} /></SwiperSlide>);
-  //   }
-  //   return images
-  // }
+    const images = [];
+    for (let i = 0; i < imageCount; i++) {
+      images.push(<SwiperSlide className='MD-songs-carousel-images-div'><img className={className} key={i} src={imageURL} alt={`Image ${i + 1}`} /></SwiperSlide>);
+    }
+    return images
+  }
 
   // const songsCSS = 'MD-songs-test-css';
-  // const albumsAndplaylistCSS = 'MD-songs-img';
+  const albumsAndplaylistCSS = 'MD-songs-img';
 
-
+  const test = 'none'
 
   return (
     <div className="MD-body">
       <h1 className='MD-label'>Manage Discography</h1>
-      <div style={{ marginBottom: '50px' }}>
+      <div className={userSongs.length ? 'MD-section-container' : 'MD-display-none'}>
         <h3 className='MD-sub-labels'>Songs</h3>
         <div className='MD-songs-container-div'>
           <Swiper
@@ -61,15 +61,17 @@ function ManageDiscography() {
             onSwiper={(swiper) => console.log(swiper)}
           >
             <div className='MD-songs-carousel-images-div'>
-              {userSongs?.map(song =>
+              {userSongs.map(song =>
                 <SwiperSlide className='MD-songs-carousel-images-div'><img className='MD-songs-test-css' key={song.id} src={song.coverImage} alt={`Image ${song.id + 1}`} /></SwiperSlide>
               )}
+              {/* Render Jennie Pics lol */}
+              {/* {MyComponent(30, albumsAndplaylistCSS)} */}
             </div>
           </Swiper>
         </div>
       </div>
 
-      <div style={{ marginBottom: '50px' }}>
+      <div className={userPlaylists.length ? 'MD-section-container' : 'MD-display-none'}>
         <h3 className='MD-sub-labels'>Playlists</h3>
         <div className='MD-playlists-container-div'>
           <Swiper
@@ -81,28 +83,33 @@ function ManageDiscography() {
             onSwiper={(swiper) => console.log(swiper)}
           >
             <div className='MD-songs-carousel-images-div'>
+              {/* {MyComponent(30, albumsAndplaylistCSS)} */}
 
             </div>
           </Swiper>
         </div>
       </div>
 
-      <h3 className='MD-sub-labels'>Albums</h3>
-      <div className='MD-playlists-container-div'>
-        <Swiper
-          modules={[Navigation]}
-          slidesPerView={4}
-          slidesPerGroup={1}
-          navigation
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          <div className='MD-songs-carousel-images-div'>
-            {userAlbums.map(album =>
-              <SwiperSlide className='MD-songs-carousel-images-div'><img className='MD-songs-img' key={album.id} src={album.coverImage} alt={`Image ${album.id + 1}`} /></SwiperSlide>
-            )}
-          </div>
-        </Swiper>
+      <div className={userAlbums.length ? 'MD-section-container' : 'MD-display-none'}>
+        <h3 className='MD-sub-labels'>Albums</h3>
+        <div className='MD-playlists-container-div'>
+          <Swiper
+            modules={[Navigation]}
+            slidesPerView={4}
+            slidesPerGroup={1}
+            navigation
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <div className='MD-songs-carousel-images-div'>
+              {userAlbums.map(album =>
+                <SwiperSlide className='MD-songs-carousel-images-div'><img className='MD-songs-img' key={album.id} src={album.coverImage} alt={`Image ${album.id + 1}`} /></SwiperSlide>
+              )}
+              {/* {MyComponent(30, albumsAndplaylistCSS)} */}
+
+            </div>
+          </Swiper>
+        </div>
       </div>
     </div>
   )
