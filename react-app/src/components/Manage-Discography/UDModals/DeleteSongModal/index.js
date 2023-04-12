d17ac8562d32652066e069ee0ac793c37639dc8f
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-04-12 01:22:57 -0400

    modals done ish. the submits arent done at all but are staged for easy setup. yall are amazing to work withgit add .! gn gn g

--- react-app/src/components/Manage-Discography/UDModals/DeleteSongModal/index.js
+++ react-app/src/components/Manage-Discography/UDModals/DeleteSongModal/index.js
@@ -3,8 +3,15 @@ import React from "react";
 import './DeleteSong.css'
 
 const DeleteSong = () => {
+    const handleDelete = () => {
+        console.log('handle submit in delete song modal')
+    }
+
     return(
-        <div>ass</div>
+        <div className='DS-Main-Wrapper' >
+            <h1 className="DS-Title">ARE YOU SURE?</h1>
+            <div className="DS-Button" onClick={handleDelete}>DELETE THIS SONG</div>
+        </div>
     )
 }
 
--- react-app/src/components/Manage-Discography/UDModals/UpdateSongModal/index.js
+++ react-app/src/components/Manage-Discography/UDModals/UpdateSongModal/index.js
@@ -1,10 +1,50 @@
-import React from "react";
-
+import React, { useState } from "react";
+import ReactSelect from 'react-select';
 import './UpdateSong.css'
 
 const UpdateSong = () => {
+    const GenreOptions = [
+        { value: 'Pop', label: 'Pop' },
+        { value: 'Rock', label: 'Rock' },
+        { value: 'R&B', label: 'R&B' },
+        { value: 'Hip-Hop', label: 'Hip-Hop' },
+        { value: 'K-Pop', label: 'K-Pop' },
+        { value: 'Country', label: 'Country' },
+        { value: 'Folk', label: 'Folk' },
+        { value: 'Jazz', label: 'Jazz' },
+        { value: 'Heavy Metal', label: 'Heavy Metal' },
+        { value: 'Funk', label: 'Funk' },
+        { value: 'EDM', label: 'EDM' },
+        { value: 'Disco', label: 'Disco' },
+        { value: 'Trap', label: 'Trap' },
+        { value: 'Grunge', label: 'Grunge' },
+    ];
+
+    const [title, setTitle] = useState('')
+    const [genreValue, setGenreValue] = useState(null)
+
+    const handleUpdate = () => {
+        console.log('handle update func in update song modal')
+        console.log(title)
+        console.log(genreValue)
+    }
+
+    const handleGenreValueChange = (selectedOption) => {
+        //console.log(selectedOption.value)
+        setGenreValue(selectedOption.value);
+    }
     return(
-        <div>bass</div>
+        <div className="US-Main-Wrapper">
+            <div className="US-Title">Update Song</div>
+            <input className="US-TitleInput" type="text" placeholder={title} value={title} onChange={((e)=> setTitle(e.target.value))}/>
+            <ReactSelect 
+                className="US-Genre"
+                options={GenreOptions}
+                value={genreValue}
+                onChange={handleGenreValueChange}
+            />
+            <div className="US-Submit-Button" onClick={handleUpdate}>Update</div>
+        </div>
     )
 }
 
