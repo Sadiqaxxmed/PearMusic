fbffb5e26506f65c7e09f430d0143db1b097db99
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-04-13 15:46:33 -0400

    fixed add to queue functionality and song menu is sorta finished

--- react-app/src/components/ToolTip/index.js
+++ react-app/src/components/ToolTip/index.js
@@ -1,32 +1,48 @@
-// import React, { useState } from "react";
-// import { useSelector } from "react-redux";
-// import './UDModal.css'
-
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import './ToolTipMenu.css'
 
-
+import { thunkUserPlaylists,thunkResetPlaylists } from "../../store/playlist";
+import { func } from "prop-types";
+import { thunkAddSong } from "../../store/queue";
 
 
 function ToolTipMenu(song) {
-    const sessionUser = useSelector(state => state.session.user)
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const userId = useSelector(state => state.session.user?.id)
     const userPlaylists = Object.values(useSelector(state => state.playlists.allPlaylists))
-    
+    const songId = song.song.id
+
     const [outerDivClassName, setOuterDivClassName] = useState('TTM-Main-Wrapper');
     const [showPlaylists, setShowPlaylists] = useState(false)
 
-    function handlePopOut(){
-        if(!showPlaylists){
+    useEffect(() => {
+        dispatch(thunkUserPlaylists(userId))
+
+    }, [dispatch, userId])
+
+    // adds song to playlist (need to make menu close after action is complete)
+    function AddToPlaylist(songId,playlistId){
+        console.log('songID=>>>>>>>>>>>', songId)
+        console.log('playlistId=>>>>>>>>>>>', playlistId)
+    }
+
+    // adds song to queue (need to add functionality to close menu once a button is clikced and an action is completed but for now its fine)
+    function addToQueue(song){
+        dispatch(thunkAddSong(song.song))
+        return console.log('song added to queue')
+    }
+
+    // handles playlists menu pop out on 'add to playlist btn' click (need to add functionality to close menu once a button is clikced and an action is completed but for now its fine)
+    function handlePopOut() {
+        if (!showPlaylists) {
             setShowPlaylists(true)
-        }else{
+        } else {
             setShowPlaylists(false)
         }
     }
-
-    const handleInnerDivClick = () => {
-        setOuterDivClassName('TTM-Main-Wrapper-Hidden');
-    };
 
     return (
         <div>
@@ -38,7 +54,7 @@ function ToolTipMenu(song) {
                     <div className='TTM-AddToPlaylist' onClick={((e) => handlePopOut())}>&nbsp;Add to playlist{`->`} </div>
                 </div>
                 <div className="TTM-Btn-Wrapper"> {/* dispatch add to queue thunk */}
-                    <div className='TTM-AddToQueue' onClick={((e) => console.log('ATQ ATQ ATQ ATQ'))}>&nbsp;Add to queue</div>
+                    <div className='TTM-AddToQueue' onClick={((e) => addToQueue(song))}>&nbsp;Add to queue</div>
                 </div>
                 <div className="TTM-Btn-Wrapper" > {/* redirect to a page of jennies or a jennie only playlist */}
                     <div className='TTM-Jennie' onClick={((e) => console.log("JENNIE JENNIE JENNIE JENNIE"))}>&nbsp;JENNIE</div>
@@ -47,8 +63,12 @@ function ToolTipMenu(song) {
                     <div className='TTM-Keegster' onClick={((e) => console.log('KEEGSTER KEEGSTER KEEGSTER KEEGSTER'))}>&nbsp;KEEGSTER</div>
                 </div>
             </div>
-            <div className={`pop-out-menu${showPlaylists ? '-open': ''}`}>
-                sadasdasdasdasdasdasd
+            {/* pop out menu for user playlists to add song to a specific playlist */}
+            <div className={`pop-out-menu${showPlaylists ? '-open' : ''}`}>
+                <div className="POM-BTN-Wrapper-Title">Playlists</div>
+                {userPlaylists.map(playlist => 
+                    <div className="POM-BTN-Wrapper" key={playlist.id} onClick={((e)=>AddToPlaylist(songId,playlist.id))}>{playlist.title}</div>
+                )}
             </div>
         </div>
     )
