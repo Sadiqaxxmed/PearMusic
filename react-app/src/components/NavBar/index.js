2c48a78e06fe7ae1a224a9dbfebd8d6676fd266c
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-04-13 11:50:50 -0400

    change

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -52,15 +52,11 @@ function NavBar() {
       setCoverImage(queue[0].coverImage)
        // setAlbumTitle(queue[0].albumTitle) 
     }
-    setSongUrl(currentSong.mp3file)
-    setSongTitle(currentSong.title)
-    setSongArtist(currentSong.artistName)
-    setCoverImage(currentSong.coverImage)
     if (songUrl) {
       setToggleIcon('fa-solid fa-pause fa-xl icon-hover-pointer')
     }
     // setAlbumTitle(currentSong.albumTitle) need to pass in album title to single song
-  },[currentSong, hasPlayed])
+  },[queue[0]])
 
 // LOGIC FOR TOP RIGHT MENU OPEN
   const toggleMenu = () => {
