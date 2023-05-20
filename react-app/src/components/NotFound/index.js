28805cc59a9544cb81ef12fb59a5196d01af7e94
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-05-20 11:53:17 -0400

    testing

--- react-app/src/components/NotFound/index.js
+++ react-app/src/components/NotFound/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function NotFound() {
-  return (
-    <div className="ERR">
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
-  );
+    return (
+        <div className="ERR">
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
 }
 
 export default NotFound;
