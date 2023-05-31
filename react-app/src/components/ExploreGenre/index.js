753cf5728e895aff07bacd28339e6f6c53ebf22d
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-05-31 18:03:47 -0400

    genre page time formatting fixed

--- react-app/src/components/ExploreGenre/index.js
+++ react-app/src/components/ExploreGenre/index.js
@@ -100,8 +100,12 @@ function ExploreGenre() {
               <img className="SG-PL-Img" alt='temp' src={exploreGenre[0]?.coverImage}></img>
             </div>
             <div className="SGPL-Top-Right">
-              <h3 className="SGPL-Title">{genre_type}</h3>
-              <p className="SGPL-Info">{exploreGenre.length} SONGS • {totalPlayTime(exploreGenre)}</p>
+              {!isMobile && <h3 className="SGPL-Title">{genre_type}</h3>}
+              {!isMobile && <p className="SGPL-Info">{exploreGenre.length} SONGS • {totalPlayTime(exploreGenre)}</p>}
+              {isMobile && <div className="SGPL-Title-Wrap">
+                <h3 className="SGPL-Title">{genre_type}</h3>
+                <p className="SGPL-Info">{exploreGenre.length} SONGS • {totalPlayTime(exploreGenre)}</p>
+              </div>}
               <p className="SGPL-Description">{descriptions[genre_type]}</p>
               <div className="SGPL-Buttons">
                 <div className="SGPL-Play-Button" onClick={(() => addQueue())}>
@@ -124,13 +128,13 @@ function ExploreGenre() {
                     <div className="SGPL-Bottom-Song">
                       <img className="SG-Bottom-PL-Img" alt='temp' src={song.coverImage} onClick={() => playNowFunc(song)}></img>
                       {!isMobile ? <p className="SGPL-Bottom-Song-text">{song.title}</p>
-                      :
-                      <div className="SG-Bottom-TA-Wrapper">
-                        <p className="SGPL-Bottom-Song-text">{song.title}</p>
-                        <p className="SGPL-Bottom-text" >{song.artistName}</p>
-                      </div>
+                        :
+                        <div className="SG-Bottom-TA-Wrapper">
+                          <p className="SGPL-Bottom-Song-text">{song.title}</p>
+                          <p className="SGPL-Bottom-text" >{song.artistName}</p>
+                        </div>
                       }
-                      
+
                     </div>
                   </div>
                   {!isMobile && (<div className="SGPL-Bottom-Artist-Header"> <p className="SGPL-Bottom-text" id="SGPL-Bottom-Info-Text">{song.artistName}</p> </div>)}
