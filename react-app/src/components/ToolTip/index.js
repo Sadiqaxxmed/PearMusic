725eff6adc61ae8725a4bb0736d24198b7809924
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-06-03 20:18:41 -0400

    browse ttm complete

--- react-app/src/components/ToolTip/index.js
+++ react-app/src/components/ToolTip/index.js
@@ -1,35 +1,38 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
-import './ToolTipMenu.css'
+import './ToolTipMenu.css';
 
 import { thunkUserPlaylists, thunkCreatePlaylist, thunkAddToPlaylist } from "../../store/playlist";
 import { thunkAddSong } from "../../store/queue";
 
-
 function ToolTipMenu({ song, setMenuOpen }) {
-    const dispatch = useDispatch()
-    const history = useHistory()
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const menuRef = useRef(null); // Ref for the menu itself
     const ulRef = useRef(null);
-    const userId = useSelector(state => state.session.user?.id)
-    const userPlaylists = Object.values(useSelector(state => state.playlists.allPlaylists))
-    const songId = song.id
+    const userId = useSelector(state => state.session.user?.id);
+    const userPlaylists = Object.values(useSelector(state => state.playlists.allPlaylists));
+    const songId = song.id;
 
-    const [outerDivClassName, setOuterDivClassName] = useState('TTM-Main-Wrapper-DIF');
-    const [showPlaylists, setShowPlaylists] = useState(false)
+    const [showPlaylists, setShowPlaylists] = useState(false);
 
     useEffect(() => {
-        dispatch(thunkUserPlaylists(userId))
+        dispatch(thunkUserPlaylists(userId));
 
         const handleClickOutside = (event) => {
-            if (ulRef.current && !ulRef.current.contains(event.target)) {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
                 setMenuOpen(false);
             }
         };
 
         document.addEventListener('click', handleClickOutside);
 
-    }, [dispatch, userId])
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+
+    }, [dispatch, setMenuOpen, userId]);
 
     // adds song to playlist (need to make menu close after action is complete)
     function AddToPlaylist(songId, playlistId) {
@@ -38,49 +41,68 @@ function ToolTipMenu({ song, setMenuOpen }) {
         setMenuOpen(false);
     }
 
-    // adds song to queue (need to add functionality to close menu once a button is clikced and an action is completed but for now its fine)
+    // adds song to queue (need to add functionality to close menu once a button is clicked and an action is completed but for now its fine)
     function addToQueue(song) {
-        dispatch(thunkAddSong(song))
+        dispatch(thunkAddSong(song));
         return;
     }
 
     const createPlaylist = async (songId, userId) => {
-        const playlistId = await dispatch(thunkCreatePlaylist(songId, userId))
-        history.push(`/SinglePlaylist/${playlistId}`)
+        const playlistId = await dispatch(thunkCreatePlaylist(songId, userId));
+        history.push(`/SinglePlaylist/${playlistId}`);
     }
 
-    // handles playlists menu pop out on 'add to playlist btn' click (need to add functionality to close menu once a button is clikced and an action is completed but for now its fine)
+    // handles playlists menu pop out on 'add to playlist btn' click (need to add functionality to close menu once a button is clicked and an action is completed but for now its fine)
     function handlePopOut() {
-        if (!showPlaylists) {
-            setShowPlaylists(true)
-        } else {
-            setShowPlaylists(false)
-        }
+        setShowPlaylists(!showPlaylists);
     }
+
+    console.log(showPlaylists);
+    console.log(userPlaylists);
 
     return (
-        <div className="TTM-container-div" ref={ulRef}>
-            <div className={outerDivClassName}>
-                <div className="TTM-Btn-Wrapper"> {/* create playlist and redirect user to that new playlist */}
-                    <div className='TTM-Create-Playlist' onClick={((e) => createPlaylist(songId, userId))}>&nbsp;Create playlist</div>
+        <div className="TT-Menu-Wrapper" ref={menuRef}>
+            <div className="TT-PL-Wrapper" style={{ display: showPlaylists ? 'block' : 'none' }} ref={ulRef}>
+                <div className="TT-PL-Title">
+                    Your playlists
+                    <i
+                        className="fa-solid fa-arrow-left"
+                        id="TT-PL-Arrow"
+                        onClick={handlePopOut}
+                    />
                 </div>
-                <div className="TTM-Btn-Wrapper" > {/* open extra menu with all user playlists */}
-                    <div className='TTM-AddToPlaylist' onClick={((e) => handlePopOut())}>&nbsp;{`<-`}Add to playlist</div>
-                </div>
-                <div className="TTM-Btn-Wrapper"> {/* dispatch add to queue thunk */}
-                    <div className='TTM-AddToQueue' onClick={((e) => addToQueue(song))}>&nbsp;Add to queue</div>
-                </div>
+                {userPlaylists.map(playlist => (
+                    <div
+                        className="TT-PL-Option"
+                        key={playlist.id}
+                        onClick={() => AddToPlaylist(songId, playlist.id)}
+                    >
+                        {playlist.title}
+                    </div>
+                ))}
             </div>
-            {/* pop out menu for user playlists to add song to a specific playlist */}
-            <div className={`pop-out-menu${showPlaylists ? '-open' : ''}`}>
-                <div className="POM-BTN-Wrapper-Title">Playlists</div>
-                {userPlaylists.map(playlist =>
-                    <div className="POM-BTN-Wrapper" key={playlist.id} onClick={((e) => AddToPlaylist(songId, playlist.id))}>{playlist.title}</div>
-                )}
+            <div className="TT-Options-Wrapper" style={{ display: showPlaylists ? 'none' : 'flex' }}>
+                <div className="TT-Option-Container">
+                    {/* create playlist and redirect user to that new playlist */}
+                    <div className="TT-Option" onClick={() => createPlaylist(songId, userId)}>
+                        Create playlist
+                    </div>
+                </div>
+                <div className="TT-Option-Container">
+                    {/* open extra menu with all user playlists */}
+                    <div className="TT-Option" onClick={handlePopOut}>
+                        Add to playlist
+                    </div>
+                </div>
+                <div className="TT-Option-Container">
+                    {/* dispatch add to queue thunk */}
+                    <div className="TT-Option" onClick={() => addToQueue(song)}>
+                        Add to queue
+                    </div>
+                </div>
             </div>
         </div>
-    )
-
+    );
 }
 
-export default ToolTipMenu
+export default ToolTipMenu;
