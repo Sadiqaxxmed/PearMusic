2eb01d48fa61d1a9019952269790e67c01738b5e
Yasine Omar Benzekri <Yasineomarbenzekri@outlook.com>
2023-11-10 15:29:02 -0500

    Update songs.py
    uploaded all imgbb hosted images to aws s3, as imagebb corrupted all image files

--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -20,7 +20,7 @@ def seed_songs():
     song1 = Song(
         title='Is There Someone Else?',
         genre='Pop',
-        coverImage='https://i.ibb.co/HDgSfQz/istheresomeoneelse.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/isThereSomeoneElse.jpeg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/The+Weeknd+-+Is+There+Someone+Else_+(Audio).mp3",
         duration=3.19,
         user_id=1,
@@ -29,7 +29,7 @@ def seed_songs():
     song2 = Song(
         title='Never Been',
         genre='Rap',
-        coverImage='https://i.ibb.co/ZzsqMV9/Never-Been.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/neverBeen.jpg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Logic+-+Never+Been+(Official+Audio).mp3",
         duration=4.07,
         user_id=1,
@@ -38,7 +38,7 @@ def seed_songs():
     song3 = Song(
         title='War with Heaven',
         genre='Pop',
-        coverImage='https://i.ibb.co/YZn941j/War-With-Heaven.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/warWithHeaven.jpeg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/keshi+-+War+With+Heaven+%5BFor+Shang-Chi+Album%5D+(Official+Audio).mp3",
         user_id=1,
         duration=3.13,
@@ -47,7 +47,7 @@ def seed_songs():
     song4 = Song(
         title='Pink + White',
         genre='Alternative',
-        coverImage='https://i.ibb.co/3fs9Q0Y/Remi.webp',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/pink%2Bwhite.jpeg',
         mp3file='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Pink++White+(Live+at+Electric+Lady).mp3',
         duration=3.15,
         user_id=1,
@@ -56,7 +56,7 @@ def seed_songs():
     song5 = Song(
         title='Whats Next',
         genre='Rap',
-        coverImage='https://i.ibb.co/kXmKRRv/whatsnext.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/whatsNext.png',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Drake+-+What's+Next+(Official+Music+Video).mp3",
         duration=3.20,
         user_id=1,
@@ -65,7 +65,7 @@ def seed_songs():
     song6 = Song(
         title='XO Tour Life',
         genre='Rap',
-        coverImage='https://i.ibb.co/xSt7qRr/XO.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/xoTourLife.webp',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Lil+Uzi+Vert+-+XO+Tour+Llif3+(Official+Lyric+Video).mp3",
         duration=3.00,
         user_id=1,
@@ -74,7 +74,7 @@ def seed_songs():
     song7 = Song(
         title='OMG',
         genre='K-Pop',
-        coverImage='https://i.ibb.co/5YhMmpP/OMG.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/OMG.jpeg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/NewJeans+(%E1%84%82%E1%85%B2%E1%84%8C%E1%85%B5%E1%86%AB%E1%84%89%E1%85%B3)+'OMG'+Official+MV+(Performance+ver.1).mp3",
         duration=3.40,
         user_id=1,
@@ -83,7 +83,7 @@ def seed_songs():
     song8 = Song(
         title='How You Like That',
         genre='K-Pop',
-        coverImage='https://i.ibb.co/C2crB7d/Howyoulikethat.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/howYouLikeThat.png',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/BLACKPINK+-+'How+You+Like+That'+MV.mp3",
         duration=3.03,
         user_id=1,
@@ -92,7 +92,7 @@ def seed_songs():
     song9 = Song(
         title='Cupid',
         genre='K-Pop',
-        coverImage='https://i.ibb.co/LhrS3H4/Fifty-Fifty.png',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/butter.jpeg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/FIFTY+FIFTY+(%E1%84%91%E1%85%B5%E1%84%91%E1%85%B3%E1%84%90%E1%85%B5%E1%84%91%E1%85%B5%E1%84%91%E1%85%B3%E1%84%90%E1%85%B5)+-+'Cupid'+(TwinVer.)+Official+Lyric+Video.mp3",
         duration=2.59,
         user_id=1,
@@ -101,7 +101,7 @@ def seed_songs():
     song10 = Song(
         title='Shelter',
         genre='EDM',
-        coverImage='https://i.ibb.co/wps8Jgw/Shelter.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/shelter.jpeg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Porter+Robinson+%26+Madeon+-+Shelter+(Official+Audio).mp3",
         duration=3.37,
         user_id=1,
@@ -110,7 +110,7 @@ def seed_songs():
     song11 = Song(
         title='Hypeboy',
         genre='K-Pop',
-        coverImage='https://i.ibb.co/3YqkZHb/HypeBoy.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/HypeBoy.webp',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/NewJeans+(%E1%84%82%E1%85%B2%E1%84%8C%E1%85%B5%E1%86%AB%E1%84%89%E1%85%B3)+-+Hype+Boy+Official+Audio.mp3",
         duration=3.37,
         user_id=1,
@@ -119,7 +119,7 @@ def seed_songs():
     song12 = Song(
         title='Butter',
         genre='K-Pop',
-        coverImage='https://i.ibb.co/2KWh2RV/Butter.png',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/butter.jpeg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/BTS+(%E1%84%87%E1%85%A1%E1%86%BC%E1%84%90%E1%85%A1%E1%86%AB%E1%84%89%E1%85%A9%E1%84%82%E1%85%A7%E1%86%AB%E1%84%83%E1%85%A1%E1%86%AB)+-+Butter+(Official+Audio).mp3",
         duration=3.00,
         user_id=1,
@@ -128,7 +128,7 @@ def seed_songs():
     song13 = Song(
         title='Kill This Love',
         genre='K-Pop',
-        coverImage='https://i.ibb.co/MBsKjbv/killthislove.webp',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/killThisLove.jpeg',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/BLACKPINK+-+'Kill+This+Love'+MV.mp3",
         duration=3.13,
         user_id=1,
@@ -137,7 +137,7 @@ def seed_songs():
     song14 = Song(
         title='Hurt From Mercury',
         genre='R&B',
-        coverImage='https://i.ibb.co/PCfdmzQ/mercury.jpg',
+        coverImage='https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/Hurt+From+Mercury.png',
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Tory+Lanez+-+Hurt+From+Mercury+%5BOfficial+Audio%5D.mp3",
         duration=3.23,
         user_id=1,
@@ -146,7 +146,7 @@ def seed_songs():
     song15 = Song(
         title="550's & Leon Dore",
         genre='R&B',
-        coverImage='https://i.ibb.co/t2vKC1k/550.jpg',
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/550's.jpg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Karri+-+550s+%26+Leon+Dore+(Official+Visualizer).mp3",
         duration=2.03,
         user_id=1,
@@ -155,7 +155,7 @@ def seed_songs():
     song16 = Song(
         title="Major ft. Key Glock",
         genre='R&B',
-        coverImage="https://i.ibb.co/WDcRZ84/2.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/Major.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Young+Dolph+-+Major+(Official+Music+Video)+ft.+Key+Glock.mp3",
         duration=4.53,
         user_id=1,
@@ -164,7 +164,7 @@ def seed_songs():
     song17 = Song(
         title="Talking To My Scale",
         genre='Rap',
-        coverImage="https://i.ibb.co/vm2qtkr/1.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/talking+to+my+scale.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Young+Dolph+-+Talking+To+My+Scale+(Official+Video).mp3",
         duration=4.46,
         user_id=1,
@@ -173,7 +173,7 @@ def seed_songs():
     song18 = Song(
         title="Get Down",
         genre='Alternative',
-        coverImage="https://i.ibb.co/vBSTKHY/Screenshot-2023-05-30-at-6-38-16-PM.png",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/getDown.png",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Still+Woozy+-+Get+Down+(Lyric+Video).mp3",
         duration=2.50,
         user_id=1,
@@ -182,7 +182,7 @@ def seed_songs():
     song19 = Song(
         title="Blonde Boyz",
         genre='Rap',
-        coverImage="https://i.ibb.co/7NvgQHj/maxresdefault.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/BlondeBoyz.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Blonde+Boyz++Cyndago+Original+Music+Video.mp3",
         duration=4.40,
         user_id=1,
@@ -191,7 +191,7 @@ def seed_songs():
     song20 = Song(
         title="My 2 Lovely Uncles (ft. Oney)",
         genre='Alternative',
-        coverImage="https://i.ibb.co/w6L5wSz/uncles.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/my2Uncles.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/SuperMega+-+My+2+Lovely+Uncles+(ft.+Oney)++(Official+Video).mp3",
         duration=3.53,
         user_id=1,
@@ -200,7 +200,7 @@ def seed_songs():
     song21 = Song(
         title="Flower St",
         genre='Alternative',
-        coverImage="https://i.ibb.co/Pg3WJv3/zen.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/flowerSt.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Flower+St.mp3",
         duration=2.34,
         user_id=1, 
@@ -209,7 +209,7 @@ def seed_songs():
     song22 = Song(
         title="Godly Behaviour",
         genre='Pop',
-        coverImage="https://i.ibb.co/hLVDyn3/https-images-genius-com-838c7bae22a3b643af85c2370b57ad5d-1000x1000x1.png",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/godlyBehavior.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/ELIO+-+Godly+Behaviour.mp3",
         duration=2.38,
         user_id=1,
@@ -218,7 +218,7 @@ def seed_songs():
     song23 = Song(
         title="Thunder",
         genre='Alternative',
-        coverImage="https://i.ibb.co/s100DQx/Screenshot-2023-05-30-at-6-25-06-PM.png",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/Thunder.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Thunder.mp3",
         duration=5.13,
         user_id=1,
@@ -227,7 +227,7 @@ def seed_songs():
     song24 = Song(
         title="I DONT KNOW ABOUT HIM",
         genre='Alternative',
-        coverImage="https://i.ibb.co/5MBYd0F/maxresdefault.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/Dont+know+about+Him.jpg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/I+DONT+KNOW+ABOUT+HIM.mp3",
         duration=3.58,
         user_id=1,
@@ -236,7 +236,7 @@ def seed_songs():
     song25 = Song(
         title="Do U Mind? (Leave the Light On)",
         genre='Alternative',
-        coverImage="https://i.ibb.co/qJNHNPj/Screenshot-2023-05-30-at-6-35-01-PM.png",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/leave+the+light+on.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Fleece+-+Do+U+Mind_+(Leave+the+Light+On)+Official+Video.mp3",
         duration=3.53,
         user_id=1,
@@ -245,7 +245,7 @@ def seed_songs():
     song26 = Song(
         title="SAY THAT YOU MISS ME",
         genre='Alternative',
-        coverImage="https://i.ibb.co/hycV6dm/Screenshot-2023-05-30-at-6-32-18-PM.png",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/sayThatYouMissMe.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/MICKEY+DARLING++SAY+THAT+YOU+MISS+ME+(audio).mp3",
         duration=3.48,
         user_id=1,
@@ -254,7 +254,7 @@ def seed_songs():
     song27 = Song(
         title="Young",
         genre='Alternative',
-        coverImage="https://i.ibb.co/kBGjDKb/Screenshot-2023-05-30-at-6-32-50-PM.png",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/young.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Frankie+Cosmos+_Young_+Official+Single.mp3",
         duration=2.02,
         user_id=1,
@@ -263,7 +263,7 @@ def seed_songs():
     song28 = Song(
         title="amphetamine",
         genre='Alternative',
-        coverImage="https://i.ibb.co/X4sjg0x/hq72012.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/amphetamine.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/boylife+-+amphetamine+(official+audio).mp3",
         duration=2.00,
         user_id=1,
@@ -272,7 +272,7 @@ def seed_songs():
     song29 = Song(
         title="lowlife w kenny beats",
         genre='Alternative',
-        coverImage="https://i.ibb.co/brxGYRg/maxresdefault121.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/lowlife.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/lowlife+w+kenny+beats+-+not+dvr.mp3",
         duration=2.27,
         user_id=1,
@@ -281,7 +281,7 @@ def seed_songs():
     song30 = Song(
         title="25MPH",
         genre='Alternative',
-        coverImage="https://i.ibb.co/xH0w39V/hq720123123.jpg",
+        coverImage="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/images/25mph.jpeg",
         mp3file="https://pearbucket.s3.us-east-2.amazonaws.com/pearmp3s/Public+Library+Commute+-+25MPH.mp3",
         duration=3.03,
         user_id=1,
